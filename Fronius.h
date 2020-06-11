69761dbf386d93146f849d8a2f5bca805d99788b
mike-2x <66127079+mike-2x@users.noreply.github.com>
2020-06-11 16:00:52 +0200

    Revamped the parser and added a copyright notice

--- Fronius.h
+++ Fronius.h
@@ -1,3 +1,21 @@
+/* 
+ * C Fronius library
+ * Copyright (c) 2020 Danielcplusplus and mike-2x.
+ * DISCLAIMER: This project is not affiliated with, funded by, or in any way associated with Fronius International GmbH.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
