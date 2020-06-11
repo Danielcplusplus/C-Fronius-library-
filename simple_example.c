fbd37a4399519d6b10ecf0bf5b8ab4b77742ffb2
Danielcplusplus <66086450+Danielcplusplus@users.noreply.github.com>
2020-06-11 17:13:07 +0200

    Update simple_example.c

--- simple_example.c
+++ simple_example.c
@@ -23,7 +23,7 @@
 double powersum = 0.0;
 int main()
 {
-	powersum = get_solar_power("YOUR_IP_ADDRESS");//"get_solar_power returns the produced/used power as a double
+	powersum = get_solar_power("ENTER_THE_IP_OF_YOUR_INVERTER");//"get_solar_power returns the produced/used power as a double
 	if(powersum < 1){
 		get_request_without("REPLACE_THIS_WITH_AN_URL_TO_MAKE_A_GET_REQUEST_TO");//This line runs a get request to an url(for example ifttt) and ignores the feedback
 		printf("%lf W are beeing fed in to the grid",fabs(powersum));
